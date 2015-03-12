ad621b87f655e5ed31a9eb2f60926960283c8c89
Melvyn <xtrmepower@gmail.com>
2015-03-12 22:53:32 +0800

    Converted everything into OOP. Unable to transfer anything larger than 1MB.

--- FileSender.java
+++ FileSender.java
@@ -8,26 +8,12 @@ import java.util.zip.CRC32;
 class FileSender {
 
     private DatagramSocket _socket;
-    private DatagramPacket _packet;
-    private InetAddress _serverAddress;
-    private int _serverPort;
-    private FileInputStream _fis;
-    private BufferedInputStream _bis;
-    private int _totalFileSize;
-    private int _payloadSize;
-    private ByteBuffer _packetBuffer;
-    private ByteBuffer _tempBuffer;
-
-    private final static int PACKET_SIZE = 1000;
-    private final static int MAX_FILENAME_LENGTH = 100; // 1 byte per alphabet
-    private final static int TOTAL_FILESIZE_BYTE_LENGTH = 8;  // size of long
-    private final static int PAYLOAD_FILESIZE_BYTE_LENGTH = 8;   // size of long
-    private final static int CHECKSUM_BYTE_LENGTH = 8;  // size of long
+    private InetAddress _address;
+    private int _port;
 
     private final static String HOSTNAME = "localhost";
 
     public static void main(String[] args) {
-
         // Check if the number of command line argument is 3
         if (args.length != 3) {
             System.out.println("Usage: java FileSender <path/filename> "
@@ -45,31 +31,21 @@ class FileSender {
         return paddedName.getBytes();
     }
 
-    private void flushByteBuffer(ByteBuffer bb, int bbsize) {
-        bb.clear();
-        bb.put(new byte[bbsize]);
-        bb.clear();
-    }
-
-    private long getChecksum(byte[] data) {
-        CRC32 checksum = new CRC32();
-
-        checksum.update(data);
-
-        return checksum.getValue();
-    }
-
-    private void appendPacketBuffer(ByteBuffer buffer, long data) {
-        buffer.putLong(data);
-    }
-
-    private void appendPacketBuffer(ByteBuffer buffer, byte[] data) {
-        buffer.put(data);
-    }
-
     public FileSender(String fileToOpen, String host, String port, String rcvFileName) {
+
+        FileInputStream fis;
+        BufferedInputStream bis;
+
+        long totalFileSize = 0;
+        long payloadSize = 0;
+        long seqNo = 0;
+        byte[] payloadData = new byte[PayloadPacket.getMaxPayloadSize()];
+
+        DatagramPacket pkt = null;
+
         try {
-            _serverAddress = InetAddress.getByName(host);
+            _address = InetAddress.getByName(host);
+            _port = Integer.parseInt(port);
             _socket = new DatagramSocket();
         } catch (UnknownHostException e) {
             System.out.println(e.toString());
@@ -77,78 +53,67 @@ class FileSender {
             System.out.println(e.toString());
         }
 
-        _serverPort = Integer.parseInt(port);
-        _fis = null;
-        _bis = null;
-        _payloadSize = 0;
-        _totalFileSize = 0;
-        byte[] fileDataBuffer = new byte[PACKET_SIZE -
-                                      TOTAL_FILESIZE_BYTE_LENGTH -
-                                      PAYLOAD_FILESIZE_BYTE_LENGTH -
-                                      CHECKSUM_BYTE_LENGTH -
-                                      MAX_FILENAME_LENGTH];
-        _tempBuffer = ByteBuffer.wrap(new byte[PACKET_SIZE-CHECKSUM_BYTE_LENGTH]);
-        _packetBuffer = ByteBuffer.wrap(new byte[PACKET_SIZE]);
+        fis = null;
+        bis = null;
 
         try {
-            _fis = new FileInputStream(fileToOpen);
-            _totalFileSize = _fis.available();
+            fis = new FileInputStream(fileToOpen);
+            bis = new BufferedInputStream(fis);
 
-            System.out.println("filesize = " + _totalFileSize);
+            totalFileSize = fis.available();
+
+            System.out.println("filesize = " + totalFileSize);
         } catch (FileNotFoundException e) {
             System.out.println(e.toString());
         } catch (IOException e) {
             System.out.println(e.toString());
         }
 
-        // Send the actual file itself.
-        _bis = new BufferedInputStream(_fis);
+        // Create file header packet.
+        FileHeaderPacket fhPkt = new FileHeaderPacket(_port);
+        fhPkt.setFileName(rcvFileName);
+        fhPkt.setTotalFileSize(totalFileSize);
+        pkt = fhPkt.createPacket();
+
+        // Send it.
         try {
-            _payloadSize = _bis.read(fileDataBuffer);
+            _socket.send(pkt);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
 
-            while (_payloadSize > 0) {
-                //appendPacketBuffer(_packetBpadFileName(rcvFileName));
-                //appendPacketBuffer(_totalFileSize);
-                //appendPacketBuffer(_payloadSize);
-                _tempBuffer.put(padFileName(rcvFileName));   // received file name
-                _tempBuffer.putLong(_totalFileSize);    // total file size
-                _tempBuffer.putLong(_payloadSize);    // payload size (amt of file data)
-                _tempBuffer.put(fileDataBuffer);  // actual payload data
+        PayloadPacket payloadPkt;
+        try {
+            payloadSize = bis.read(payloadData);
 
-                // Perform checksum on the data and put it into the packet buffer.
-                long checksum = getChecksum(_tempBuffer.array());
-                System.out.println("checksum="+checksum);
-                _packetBuffer.putLong(checksum);
-                _packetBuffer.put(_tempBuffer.array());
+            while (payloadSize > 0) {
+                // Create payload packet.
+                payloadPkt = new PayloadPacket(_port, seqNo);
 
-                // Create a new packet with the data in the buffer.
-                _packet = new DatagramPacket(_packetBuffer.array(), PACKET_SIZE, _serverAddress, _serverPort);
+                payloadPkt.setPayloadSize(payloadSize);
+                payloadPkt.setPayloadData(payloadData);
 
-                // Send the packet via the client side's socket.
-                _socket.send(_packet);
+                pkt = payloadPkt.createPacket();
 
-                // Continue reading the file for more data.
-                fileDataBuffer = new byte[PACKET_SIZE -
-                                          TOTAL_FILESIZE_BYTE_LENGTH -
-                                          PAYLOAD_FILESIZE_BYTE_LENGTH -
-                                          CHECKSUM_BYTE_LENGTH -
-                                          MAX_FILENAME_LENGTH];
-                _payloadSize = _bis.read(fileDataBuffer);
+                _socket.send(pkt);
 
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    System.out.println(e.toString());
-                }
+                // Clean up a little.
+                pkt = null;
+                payloadPkt = null;
+                payloadData = null;
 
-                flushByteBuffer(_packetBuffer, PACKET_SIZE);
-                flushByteBuffer(_tempBuffer, PACKET_SIZE-CHECKSUM_BYTE_LENGTH);
+                // Read for more data.
+                payloadData = new byte[PayloadPacket.getMaxPayloadSize()];
+                payloadSize = bis.read(payloadData);
+
+                // Increment sequence number.
+                seqNo++;
             }
         } catch (IOException e) {
             System.out.println(e.toString());
         } finally {
             try {
-                _bis.close();
+                bis.close();
                 _socket.close();
             } catch (IOException e) {
                 System.out.println(e.toString());
